d1539f547e1505b92d9bae645bd29379d78d0ca2
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2026-02-09 18:50:09 +0100

    Create password gen logic and form to pass using GET the pass length

---
+++ index.php
@@ -0,0 +1,66 @@
+
+<?php 
+
+    $passwordLength = $_GET['length'];
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    <title>Password Generator</title>
+</head>
+<body>
+
+    <div class="d-flex flex-column align-items-center">
+        <form action="" method="GET" >
+         <div class="mb-3 mt-5">
+             <label for="exampleInputPassword1" class="form-label">Password length</label>
+             <input type="number" class="form-control" id="exampleInputPassword1" name="length" min='8' max='30' >
+         </div>
+            <!-- <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1" name="parking">
+                <label class="form-check-label" for="exampleCheck1">Parking</label>
+            </div> -->
+            <button type="submit" class="btn btn-primary" >Submit</button>
+        </form>
+
+        <?php
+        
+            echo "$passwordLength <br>";
+
+            function passwordGen( int $length) : string {
+
+                $characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%&";
+                $password ='';
+
+                for ($i=0; $i <= $length ; $i++) { 
+                    $password .= $characters[random_int(0, strlen($characters) -1 )];
+                }
+
+                return $password;
+
+            }
+
+
+            echo passwordGen($passwordLength)
+        ?>
+
+    </div>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
